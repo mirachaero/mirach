fbb8ddf921140e24a98135f669c8b518d38be8f9
blacksof-dev <dev.blacksof@gmail.com>
2025-11-26 17:39:09 +0530

    fix: font issue

--- app/home/banner.tsx
+++ app/home/banner.tsx
@@ -15,7 +15,7 @@ export const Banner = () => {
       </div>
 
       <div className=" z-10   absolute bottom-8 md:bottom-30  w-container text-center text-white flex flex-col items-center   ">
-        <h1 className="custom-text-64 font-medium   animate-fade-up-slow ">
+        <h1 className="custom-text-64 font-medium  font-neue-montreal animate-fade-up-slow ">
           Airborne innovation <br className="block md:hidden" /> with precision
         </h1>
         <p className="custom-text-24 font-normal mt-2 md:mt-4  max-w-lg text-gray-200 animate-fade-up-slower">
